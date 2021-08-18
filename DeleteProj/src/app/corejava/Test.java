52ee1d5b9d50b462664628916551aa3061a8d3da
BhaskarReddy0608 <tbaskarreddy@gmail.com>
2021-08-18 14:44:51 +0530

    addng a and b element to c.

--- DeleteProj/src/app/corejava/Test.java
+++ DeleteProj/src/app/corejava/Test.java
@@ -6,6 +6,9 @@ public class Test {
 		System.out.println("welcome");
 		System.out.println("hi");
 		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
     //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
 	}
 
